8f09515405f646aac3cede95bc99e982a05ca7ff
SmalRat <30555080+SmalRat@users.noreply.github.com>
2025-04-03 01:14:57 +0300

    fix(benchmark thread pinning): fix thread pinning for non-obvious numa nodes numerations

--- benchmark/worker.jl
+++ benchmark/worker.jl
@@ -4,6 +4,7 @@ using ArgParse
 using Dates
 using UUIDs
 using ThreadPinning
+using Hwloc
 
 using BenchmarkTools
 using BenchmarkPlots, StatsPlots
@@ -59,11 +60,25 @@ function parse_args(raw_args)
 end
 
 function do_pin_threads()
-    println("Pinning Julia threads to CPU threads")
-    cpu_threads = numa(1)
+    @info "Pinning Julia threads to CPU threads"
+    cpu_threads::Vector{Int} = []
+    i = 0
+    n_numa_nodes = num_numa_nodes()
+
+    for i in 1:n_numa_nodes
+        cpu_threads = numa(i)
+        if length(cpu_threads) != 0
+            @info "Using NUMA node $(i-1)"
+            break
+        end
+    end
+    if length(cpu_threads) == 0
+        throw(ErrorException("No NUMA node with CPUs found, exiting..."))
+    end
+
     num_julia_threads = Threads.nthreads()
     if (num_julia_threads > length(cpu_threads))
-        println("Warning: number of Julia threads ($num_julia_threads) is greater than allocated CPU threads ($cpu_threads). Oversubscribing.")
+        @warn "Warning: number of Julia threads ($num_julia_threads) is greater than allocated CPU threads ($cpu_threads). Oversubscribing."
         mult_factor = ceil(num_julia_threads / length(cpu_threads))
         cpu_threads = repeat(cpu_threads, Int(mult_factor))
     end
